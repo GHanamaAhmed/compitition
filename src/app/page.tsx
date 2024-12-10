81e183dbc7a7dd8ac08e6c2413681f950b3c6564
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 <ghanamaahmed@gmail.com>
2024-12-10 19:06:17 +0100

    f

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
 import { LoginForm } from "@/components/login-form";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-export default function Page() {
+export default async function Page() {
+  const { userId } = await auth();
+  if (userId) {
+    return redirect("/dashboard");
+  }
   return (
     <div className="flex h-screen w-full items-center justify-center px-4">
       <LoginForm />
